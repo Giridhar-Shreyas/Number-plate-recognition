273abe1ddc8ab2f5b82b52d2f00276b479e2c983
Shreyas <shreyas.giridhar@aalto.fi>
2024-05-23 12:06:59 +0300

    refactored code

--- utils.py
+++ utils.py
@@ -5,9 +5,13 @@ import matplotlib.patches as patches
 from PIL import Image
 
 
-def draw_pose(image, lables):
-    with open(lables) as fp:
-        data = [list(map(float, line.strip().split(' '))) for line in fp]
+def draw_pose(image, data):
+    """
+    Args: 
+        image (str): Path to the image with the resolution 640x640
+        data list[float]: A list of exactly 5 values with format [class, anchor x, anchor y, width, height]
+    """
+
     im = Image.open(image)
     fig, ax = plt.subplots()
     ax.imshow(im)
@@ -31,16 +35,51 @@ def test_draw_pose():
         if(i > 10):
             break
         if(k % 3 == 0):
-            draw_pose(path_img+'/'+img, path_label+'/'+label)
+            with open(path_label+'/'+label) as fp:
+                data = [list(map(float, line.strip().split(' '))) for line in fp]
+            draw_pose(path_img+'/'+img, data)
             i+=1
         k+=1
 
 
 
-def convert_Yolo8(filePath):
-    with open(filePath, 'r') as file:
-        file_content = file.read()
-    doc = parse(filePath)
-    print(doc.getElementsByTagName("")[0].firstChild.nodeValue)
-    
+def convert_Yolo8():
 
+    for i in range(249):
+        ii = i+1
+        img = os.getcwd() + '/data/data_1/images/N'+str(ii)+'jpeg'
+        label = os.getcwd() + '/data/data_1/images/N'+str(ii)+'xml'
+        rezise_img(img)
+
+ 
+        doc = parse(label)
+        print(doc.getElementsByTagName("")[0].firstChild.nodeValue)
+
+
+def rezise_img(imgFile):
+    """
+    Args:
+        imgFile (str): Path to the image needed to be resized 
+    """
+    img = imgFile
+    image = Image.open(img)
+    image = image.resize((640,640))
+    image.save(imgFile)
+
+def convert_label(label):
+    """
+    Args:
+        label (str): File path to the label in xml format
+        key tags: Height, Width, bounding box (xmin, ymin, xmax, ymax) 
+    """
+
+    doc = parse(label)
+    resHeight = doc.getElementsByTagName("height")[0].firstChild.nodeValue
+    resWidth = doc.getElementsByTagName("width")[0].firstChild.nodeValue
+    xmin = doc.getElementsByTagName("xmin")[0].firstChild.nodeValue
+    ymin = doc.getElementsByTagName("ymin")[0].firstChild.nodeValue
+    xmax = doc.getElementsByTagName("xmax")[0].firstChild.nodeValue
+    ymax = doc.getElementsByTagName("ymax")[0].firstChild.nodeValue
+
+    #xcent = 
+    #ycent = 
