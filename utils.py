d7b2f1b1dc63e003f4bb364524e0c845e5e35278
Shreyas <shreyas.giridhar@aalto.fi>
2024-05-23 14:25:39 +0300

    finished data manuplation

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ def draw_pose(image, data):
     """
     Args: 
         image (str): Path to the image with the resolution 640x640
-        data list[float]: A list of exactly 5 values with format [class, anchor x, anchor y, width, height]
+        data list[[float]]: A 2-d list of exactly 5 values with format [class, x center, y center, width, height]
     """
 
     im = Image.open(image)
@@ -29,8 +29,8 @@ def draw_pose(image, data):
 def test_draw_pose():
     i = 0
     k = 0
-    path_img = os.getcwd() + '/data/data_2_yolo_8/train/images'
-    path_label = os.getcwd() + '/data/data_2_yolo_8/train/labels'
+    path_img = os.getcwd() + '/data/final_data/train/images'
+    path_label = os.getcwd() + '/data/final_data/train/labels'
     for img, label in zip(os.listdir(path_img), os.listdir(path_label)):
         if(i > 10):
             break
@@ -47,39 +47,55 @@ def convert_Yolo8():
 
     for i in range(249):
         ii = i+1
-        img = os.getcwd() + '/data/data_1/images/N'+str(ii)+'jpeg'
-        label = os.getcwd() + '/data/data_1/images/N'+str(ii)+'xml'
-        rezise_img(img)
+        img = os.getcwd() + '/data/data_1/images/N'+str(ii)+'.jpeg'
+        label = os.getcwd() + '/data/data_1/images/N'+str(ii)+'.xml'
+        rezise_img(img, os.getcwd() + '/data/final_data/train/images/N'+str(ii)+'.jpeg')
+        data = convert_label(label)
+        if data is not None:
+            with open(os.getcwd() + '/data/final_data/train/lables/N'+str(ii)+'.txt','w') as f:
+                f.write(data)
 
- 
-        doc = parse(label)
-        print(doc.getElementsByTagName("")[0].firstChild.nodeValue)
 
-
-def rezise_img(imgFile):
+def rezise_img(imgFile, saveTo):
     """
     Args:
         imgFile (str): Path to the image needed to be resized 
+        saveTo (str): Path to where the image needs to be be saved
     """
     img = imgFile
-    image = Image.open(img)
-    image = image.resize((640,640))
-    image.save(imgFile)
+    try:
+        image = Image.open(img)
+        image = image.resize((640,640))
+        image.save(saveTo)
+    except OSError as e:
+        pass
+
+    
 
 def convert_label(label):
     """
     Args:
         label (str): File path to the label in xml format
         key tags: Height, Width, bounding box (xmin, ymin, xmax, ymax) 
+    Returns:
+        lables in yolo format and 
     """
+    try:
+        doc = parse(label)
+        resHeight = float(doc.getElementsByTagName("height")[0].firstChild.nodeValue)
+        resWidth = float(doc.getElementsByTagName("width")[0].firstChild.nodeValue)
+        xmin = float(doc.getElementsByTagName("xmin")[0].firstChild.nodeValue)/resWidth
+        ymin = float(doc.getElementsByTagName("ymin")[0].firstChild.nodeValue)/resHeight
+        xmax = float(doc.getElementsByTagName("xmax")[0].firstChild.nodeValue)/resWidth
+        ymax = float(doc.getElementsByTagName("ymax")[0].firstChild.nodeValue)/resHeight
 
-    doc = parse(label)
-    resHeight = doc.getElementsByTagName("height")[0].firstChild.nodeValue
-    resWidth = doc.getElementsByTagName("width")[0].firstChild.nodeValue
-    xmin = doc.getElementsByTagName("xmin")[0].firstChild.nodeValue
-    ymin = doc.getElementsByTagName("ymin")[0].firstChild.nodeValue
-    xmax = doc.getElementsByTagName("xmax")[0].firstChild.nodeValue
-    ymax = doc.getElementsByTagName("ymax")[0].firstChild.nodeValue
+        width = xmax-xmin
+        height = ymax-ymin
+        xcent = xmin+(width/2.0)
+        ycent = ymin+(height/2.0)
 
-    #xcent = 
-    #ycent = 
+        return str(0)+' '+str(xcent)+' '+str(ycent)+' '+str(width)+' '+str(height)
+    except OSError as e:
+        return None
+
+test_draw_pose()
